4df0b54535f7827217b655f33ff0627431c3875e
Orbitroll <orbitroll@gmail.com>
2025-08-14 21:30:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print('Wellcome to Moshe Pizza!')
+name = input('Please enter your name: ')
+order = input(f'Dear {name},where do you want to sit? : ')
+while not 'inside' in order and not 'outside' in order:
+    order = input(f'Dear {name},please tell me where you want to sit? : ')
