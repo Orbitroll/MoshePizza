d5a5e64629ddf7720be1ed3453ed243bf15dc488
Orbitroll <orbitroll@gmail.com>
2025-08-14 21:38:49 +0300

    shlomi
    baby tell me one more time

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ name = input('Please enter your name: ')
 order = input(f'Dear {name},where do you want to sit? : ')
 while not 'inside' in order and not 'outside' in order:
     order = input(f'Dear {name},please tell me where you want to sit? : ')
+if 'inside' in order:
+    print(f'Great! We have a nice table for you inside, {name}.')
+elif 'outside' in order:
+    print(f' {name} its cold outside but do whatever you want.')
