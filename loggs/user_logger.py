e82d720f37dd3cc30aa9ba4bfaf72b3e7242ef99
MAK-XI <mak19.bm@gmail.com>
2025-09-14 19:32:06 +0300

    frquent user added

--- loggs/user_logger.py
+++ loggs/user_logger.py
@@ -1,11 +1,24 @@
 from data import order_storage
 from data import admins
+from datetime import datetime, timedelta
 
-class user :
+class user:
     def __init__(self, username, password, role) -> None:
         self.username = username
         self.password = password
         self.role = role
+        self.orders = []  # store datetime of each order
+
+    def add_order(self):
+        
+        self.orders.append(datetime.now())
+
+    def is_frequent(self):
+        
+        now = datetime.now()
+        one_month_ago = now - timedelta(days=30)
+        recent_orders = [o for o in self.orders if o >= one_month_ago]
+        return len(recent_orders) >= 3
 
 def login():
     print('Welcome to Moshe Yavne Pizza the best pizza in Yavne')
